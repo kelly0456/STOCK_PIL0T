b3d15ca35fd5c0a9bee3dc753ae90988ff358288
kelly0456 <kevinkellyk194@gmail.comm>
2026-07-31 12:08:13 +0300

    Fix authentication and improve MongoDB connection logging

--- STOCK_PILOT/src/Pages/Sales.jsx
+++ STOCK_PILOT/src/Pages/Sales.jsx
@@ -595,7 +595,7 @@ export default function Sales() {
                         <img
                           src={
                             product.imageUrl ||
-                            "https://placehold.co/400x250?text=No+Image"
+                            "https://i.pinimg.com/736x/88/dc/1c/88dc1c3ab7fc882fd7d6e18fedb1efac.jpg"
                           }
                           alt={product.name}
                           className="card-img-top"
